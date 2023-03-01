fcf7c2bd29a8e70146a8d41803889f6561abdecf
min <mintada6069@gmail.com>
2023-03-01 14:50:38 +0700

    min

--- Program.cs
+++ Program.cs
@@ -1,17 +1,27 @@
 ﻿ using System;
 
-namespace workshop2_1 {
+namespace workshop2__1 {
     class Program {
         static void Main(string[] args) {
+        int sum = 0;
+            for(int adder = 1; adder <= 5; adder++) {
+                sum = sum + adder;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
+
+   
+        
+    
+
+
+
             
           
 
-      double  x = 7.25;
-      int y = 7;
       
-      Console.WriteLine(x + y);
-    }
-}
 
 
 	   
@@ -23,7 +33,6 @@ namespace workshop2_1 {
 
             
             
-        }
+        
 
-    }
-}
+    

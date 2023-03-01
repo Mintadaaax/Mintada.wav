69e17453ad202492c81a5e90c32bca3e484adc34
min <mintada6069@gmail.com>
2023-03-01 14:01:42 +0700

    messi

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ using System;
+
+namespace workshop2_1 {
+    class Program {
+        static void Main(string[] args) {
+            
+          
+
+      double  x = 7.25;
+      int y = 7;
+      
+      Console.WriteLine(x + y);
+    }
+}
+
+
+	   
+
+
+            
+
+        
+
+            
+            
+        }
+
+    }
+}
